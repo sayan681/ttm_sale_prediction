bf9d9e1a2d28b1940932fccf8932d2ed5dc1b1df
sayan681 <73129540+sayan681@users.noreply.github.com>
2023-03-18 01:59:25 +0530

    Update Prediction_rating.py

--- Prediction_rating.py
+++ Prediction_rating.py
@@ -1,8 +1,18 @@
+import streamlit as st
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearnex import patch_sklearn
+import plotly.express as px
+
 patch_sklearn()
+
+# Set the title and description of the Streamlit app
+st.set_page_config(page_title="Food Sales Prediction",
+                   page_icon=":pizza:", layout="wide")
+st.title("Food Sales Prediction")
+
+st.markdown("*This app predicts the number of times a new food item is likely to be sold based on its price and rating, using a more advanced and optimized KMeans clustering algorithm by Intel(R) Extension for Scikit-learn enabled (https://github.com/intel/scikit-learn-intelex)*")
 
 # Load the dataset
 data = pd.read_csv(r'food.csv')
@@ -16,17 +26,30 @@ features_scaled = scaler.fit_transform(features)
 features_scaled = pd.DataFrame(features_scaled, columns=features.columns)
 
 # Train the KMeans model
-kmeans = KMeans(n_clusters=2, init='k-means++', n_init=10, max_iter=300, algorithm='lloyd')
+kmeans = KMeans(n_clusters=2, init='k-means++', n_init=10,
+                max_iter=300, algorithm='lloyd')
 kmeans.fit(features_scaled)
 
-# Predict the cluster for a new item
-new_item = [[290, 4.8]]  # Price (in Rupees), Rating (out of 5)
+# Set up the sidebar and plot area
+st.sidebar.header('New Item Details')
+new_price = st.sidebar.number_input('Price (in Rupees)', value=290)
+new_rating = st.sidebar.slider(
+    'Rating (out of 5)', min_value=0.0, max_value=5.0, step=0.1, value=3.8)
+new_item = [[new_price, new_rating]]
 new_item_scaled = scaler.transform(new_item)
 new_item_scaled = pd.DataFrame(new_item_scaled, columns=features.columns)
 cluster = kmeans.predict(new_item_scaled)
 
 # Determine if the new item is likely to have a high number of times sold based on the cluster
 if cluster == 1:
-    print('The new item is likely to have a high number of sales.')
+    st.subheader('The new item is likely to have a high number of sales.')
 else:
-    print('The new item is not likely to have a high number of sales.')
+    st.subheader('The new item is not likely to have a high number of sales.')
+
+# Plot the data with clusters
+fig = px.scatter(features_scaled, x='Price', y='Rating', color=kmeans.labels_)
+
+st.plotly_chart(fig)
+
+
+st.markdown('*This is a prototype ML model to be used in trythemenu.com and currently running on a synthetic dataset developed by Sayan Malakar*')
